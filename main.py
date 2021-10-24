d3e043396089ee086868fc2ccfce58e7c01680fe
Cassiana <lima.cglima@gmail.com>
2021-10-24 18:08:03 -0300

    repostas API via CEP

--- main.py
+++ main.py
@@ -9,4 +9,12 @@ objeto_cep = BuscaEndereco(cep)
 # r = requests.get("http://viacep.com.br/ws/01001000/json/")
 # print(type(r.text))
 
-print(objeto_cep.acessa_via_cep())
+# a = objeto_cep.acessa_via_cep()
+# print(type(a.text))
+# print(a.text)
+# print(type(a.json()))
+# print(a.json())
+# print(a.json()['bairro'])
+
+bairro, cidade, uf = objeto_cep.acessa_via_cep()
+print(bairro, cidade, uf)
