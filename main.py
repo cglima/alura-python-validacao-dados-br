5830e3a930720a8b6028fb5d297852842e8bda41
Cassiana <lima.cglima@gmail.com>
2021-10-24 15:48:38 -0300

    manipulando datas

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from documento import Documento
+from datas_br import DatasBr
 
-# cpf = "15316264754"
-#
-# objeto_cpf = CpfCnpj(cpf)
-# print(objeto_cpf)
-
-exemplo_cnpj = "08027715000140"
-exemplo_cpf = "15316264754"
-documento = Documento.cria_documento(exemplo_cpf)
-print(documento)
+cadastro = DatasBr()
+print(cadastro.dia_semana())
