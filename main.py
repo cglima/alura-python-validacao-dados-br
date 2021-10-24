8af574a20bfd19aadba7f7d6a5ec2d57ab1247ec
Cassiana <lima.cglima@gmail.com>
2021-10-23 21:34:21 -0300

    validando cnpj

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from cpf import ValidaCPF
+from documento import CpfCnpj
 
-cpf = "15316264754"
+# cpf = "15316264754"
+#
+# objeto_cpf = CpfCnpj(cpf)
+# print(objeto_cpf)
 
-objeto_cpf = ValidaCPF(cpf)
-print(objeto_cpf)
+exemplo_cnpj = "08027715000140"
+exemplo_cpf = "15316264754"
+documento = CpfCnpj(exemplo_cpf, 'cpf')
+print(documento)
